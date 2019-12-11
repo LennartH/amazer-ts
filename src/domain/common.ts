ef2099bc3902bb54c4dc69a747ee300b9ac38c4f
Lennart Hensler <LennartHensler@gmail.com>
2019-12-11 13:26:28 +0100

    Add docu for domain/common.ts

--- src/domain/common.ts
+++ src/domain/common.ts
@@ -6,17 +6,29 @@ export interface Size {
     height: number;
 }
 
+/** Utility functions for {@link Size} */
 export namespace Size {
+    /** Format `size` as a string: `<width>x<height>` */
     export function stringify(size: Size): string {
         return `${size.width}x${size.height}`;
     }
 
+    /**
+     * Creates a new {@link Size} object from the given data.
+     * 
+     * `data` must be an object with attribute `size` or
+     * `width` **and** `height`. The value for `size` can
+     * be a string parseable by {@link fromString} or another
+     * object with `width` and `height` attributes.
+     * 
+     * @throws An error, if the `data` has no size information.
+     */
     export function fromObject(data: Dict<any>): Size {
         if (data.size !== undefined) {
             if (typeof data.size === "string") {
                 return Size.fromString(data.size);
             } else {
-                return data.size;
+                return {width: data.size.width, height: data.size.height};
             }
         } else if (data.width !== undefined && data.height !== undefined) {
             return {width: data.width, height: data.height};
@@ -25,6 +37,14 @@ export namespace Size {
         }
     }
 
+    /**
+     * Creates a new {@link Size} object from the given string.
+     * 
+     * The string must have the format `<width>x<height>`, where
+     * width and height are valid numbers.
+     * 
+     * @throws An error, if `value` does not match the format
+     */
     export function fromString(value: string): Size {
         let parts: string[] = value.split("x");
         if (parts.length != 2) {
@@ -44,13 +64,28 @@ export interface Point {
     y: number;
 }
 
+/**
+ * Extension class for {@link Point} providing additional
+ * utility methods.
+ */
 export class Vector implements Point {
     constructor(readonly x: number, readonly y: number) { }
 
+    /** Creates a new vector from the given {@link Point} */
     static from(point: Point): Vector {
         return new Vector(point.x, point.y);
     }
 
+    /**
+     * Creates a random vector with `0 <= x < size.width` and
+     * `0 <= y < size.height` that matches the given predicate.
+     * 
+     * May result in an infinite loop, if `predicate` never returns
+     * `true`.
+     * 
+     * @param size The upper bound of the resulting vector
+     * @param predicate Function that checks if a random point is valid.
+     */
     static random(size: Size, predicate?: (p: Vector) => boolean): Vector {
         let point: Vector;
         do {
@@ -59,16 +94,32 @@ export class Vector implements Point {
         return point;
     }
 
-    translate(direction: {dx?: number, dy?: number}, times=1): Vector {
-        return new Vector(this.x + (direction.dx || 0) * times, this.y + (direction.dy || 0) * times);
+    /**
+     * Translates this vector by the given delta x and delta y
+     * multiplied by `times`, creating a new vector instance.
+     * 
+     * If `by` doesn't have `dx`/`dy` 0 is used.
+     * 
+     * @param by The amount to move by 
+     * @param times Factor used to multiply `by` by
+     * 
+     * @returns A new vector instance translated by `by * times`
+     */
+    translate(by: {dx?: number, dy?: number}, times=1): Vector {
+        return new Vector(this.x + (by.dx || 0) * times, this.y + (by.dy || 0) * times);
     }
 
+    /** Calculated the euclidean distance to the given {@link Point}. */
     distance(other: Point): number {
         const dx = other.x - this.x;
         const dy = other.y - this.y;
         return Math.sqrt(dx*dx + dy*dy);
     }
 
+    /**
+     * Checks if `x` and `y` of the given {@link Point} are strictly
+     * equal to this vector.
+     */
     equals(other: Point): boolean {
         return this.x === other.x && this.y === other.y;
     }
@@ -80,6 +131,10 @@ export class Rectangle {
     readonly bottomLeft: Vector;
     readonly bottomRight: Vector;
 
+    /**
+     * @param topLeft Top left coordinates of the rectangle
+     * @param size Size of the rectangle
+     */
     constructor(topLeft: Point, readonly size: Size) {
         this.topLeft = new Vector(topLeft.x, topLeft.y);
         this.topRight = new Vector(topLeft.x + size.width - 1, topLeft.y);
@@ -87,11 +142,22 @@ export class Rectangle {
         this.bottomRight = new Vector(topLeft.x + size.width - 1, topLeft.y + size.height - 1);
     }
 
+    get width(): number {
+        return this.size.width;
+    }
+
+    get height(): number {
+        return this.size.height;
+    }
+
     intersect(other: Rectangle): boolean {
         return this.topLeft.x < other.bottomRight.x && this.bottomRight.x > other.topLeft.x
             && this.topLeft.y < other.bottomRight.y && this.bottomRight.y > other.topLeft.y;
     }
 
+    /**
+     * Generates all {@link Vector points} contained by this rectangle.
+     */
     *points(): Iterable<Vector> {
         for (let x = this.topLeft.x; x <= this.bottomRight.x; x++) {
             for (let y = this.topLeft.y; y <= this.bottomRight.y; y++) {
@@ -107,12 +173,17 @@ export class Rectangle {
     }
 }
 
+/** Utility class for the possible directions */
 export class Direction {
-    private constructor(
-        readonly name: string,
-        readonly dx: number,
-        readonly dy: number
-    ) { }
+    readonly name: string
+    readonly dx: number
+    readonly dy: number
+
+    private constructor(name: string, dx: number, dy: number) {
+        this.name = name;
+        this.dx = dx;
+        this.dy = dy;
+    }
 
     static readonly None = new Direction("None", 0, 0);
 
@@ -125,6 +196,9 @@ export class Direction {
     static readonly Left = new Direction("Left", -1, 0);
     static readonly UpLeft = new Direction("UpLeft", -1, 1);
 
+    /**
+     * @returns `true`, if the direction is strictly horizontal
+     */
     isHorizontal(): boolean {
         if (this === Direction.Left || this === Direction.Right) {
             return true;
@@ -133,6 +207,9 @@ export class Direction {
         }
     }
 
+    /**
+     * @returns `true`, if the direction is strictly vertical
+     */
     isVertical(): boolean {
         if (this === Direction.Up || this === Direction.Down) {
             return true;
@@ -141,14 +218,23 @@ export class Direction {
         }
     }
 
+    /**
+     * @returns `true`, if the direction is horizontal or vertical
+     */
     isStraight(): boolean {
         return this.isHorizontal() || this.isVertical();
     }
 
+    /**
+     * @returns `true`, if the direction is diagonal
+     */
     isDiagonal(): boolean {
         return Direction.diagonals().includes(this);
     }
 
+    /**
+     * @returns The opposite if this direction.
+     */
     opposite(): Direction {
         let index = Direction.all().indexOf(this);
         return Direction.all()[(index + 4) % 8];
@@ -173,6 +259,11 @@ export class Direction {
         Direction.UpRight, Direction.DownRight, Direction.DownLeft, Direction.UpLeft
     ]
 
+    /**
+     * @returns The direction for the given name
+     * 
+     * @throws An error, if no direction for the name could be found
+     */
     static forName(name: string): Direction {
         let direction = Direction.values().find(d => d.name === name);
         if (direction === undefined) {
@@ -181,18 +272,30 @@ export class Direction {
         return direction;
     }
 
+    /**
+     * @returns All directions, including the `None` direction
+     */
     static values(): ReadonlyArray<Direction> {
         return Direction._values;
     }
 
+    /**
+     * @returns All actual directions (without the `None` direction)
+     */
     static all(): ReadonlyArray<Direction> {
         return Direction._all;
     }
 
+    /**
+     * @returns All straight directions
+     */
     static straights(): ReadonlyArray<Direction> {
         return Direction._straights;
     }
 
+    /**
+     * @returns All diagonal directions
+     */
     static diagonals(): ReadonlyArray<Direction> {
         return Direction._diagonals;
     }
